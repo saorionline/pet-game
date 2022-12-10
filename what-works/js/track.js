d47b85af0d740d314d51fee7831c295bf8c58acb
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-10 15:26:02 -0500

    atac set

--- what-works/js/track.js
+++ what-works/js/track.js
@@ -1,6 +1,46 @@
+let gamerAtac
+let enemyPower
+
 function startGame(){
     let petPress = document.getElementById('pet-press')
 petPress.addEventListener('click', gamerOption)
+
+let waterPower = document.getElementById('water-push')
+waterPower.addEventListener('click', waterAtac)
+let firePower = document.getElementById('fire-push')
+firePower.addEventListener('click', fireAtac)
+let earthPower = document.getElementById('earth-push')
+earthPower.addEventListener('click', earthAtac)
+let airPower = document.getElementById('air-storm')
+airPower.addEventListener('click', airAtac)
+let eterPower = document.getElementById('eter-soft')
+eterPower.addEventListener('click', eterAtac)
+let gasPower = document.getElementById('gas-release')
+gasPower.addEventListener('click', gasAtac)
+}
+
+function startEnemyAtac() {
+    let randomAtac = randomOption(1 ,6)
+
+    if (randomAtac ==1) {
+        enemyPower = 'Water'
+        alert ("Your enemy attacked you with " + enemyPower)
+    } else if(randomAtac == 2) {
+        enemyPower = 'Fire'
+        alert ("Your enemy attacked you with " + enemyPower) 
+    } else if(randomAtac == 3) {
+        enemyPower = 'Earth'
+        alert ("Your enemy attacked you with " + enemyPower)
+    } else if(randomAtac == 4) {
+        enemyPower = 'Air'
+        alert ("Your enemy attacked you with " + enemyPower)
+    } else if(randomAtac == 5) {
+        enemyPower = 'Eter'
+        alert ("Your enemy attacked you with " + enemyPower)
+    } else {
+        enemyPower = 'Gas'
+        alert ("Your enemy attacked you with " + enemyPower)
+    }
 }
 
 function gamerOption() {
@@ -10,43 +50,80 @@ function gamerOption() {
     let inputLangos = document.getElementById('langos')
     let inputTucapalma = document.getElementById('tucapalma')
     let inputPydos = document.getElementById('pydos')
-    let sayGamerPet = document.getElementById('gamer-pet')
+    let setGamerPet = document.getElementById('gamer-pet')
 
    if(inputHipogoge.checked) {
-    sayGamerPet.innerHTML = 'Hipogoge'
+    setGamerPet.innerHTML = 'Hipogoge'
    } else if (inputCapi.checked) {
-    sayGamerPet.innerHTML = 'Capi'
+    setGamerPet.innerHTML = 'Capi'
    } else if (inputRatigueya.checked) {
-    sayGamerPet.innerHTML = 'Ratigueya'
+    setGamerPet.innerHTML = 'Ratigueya'
    } else if (inputLangos.checked) {
-    sayGamerPet.innerHTML = 'Langos'
+    setGamerPet.innerHTML = 'Langos'
    } else if (inputTucapalma.checked) {
-    sayGamerPet.innerHTML = 'Tucapalma'
+    setGamerPet.innerHTML = 'Tucapalma'
    } else if (inputPydos.checked) {
-    sayGamerPet.innerHTML = 'Pydos'
+    setGamerPet.innerHTML = 'Pydos'
    } else {
     alert ('Please select a Pet')
    }  
-   enemysPet()
+   enemyPet()
 }
 
-function enemysPet(){
-    let randomAtack = randomOption(1 ,6)
-    let sayEnemysPet = document.getElementById('opponent-pet')
+function enemyPet(){
+    let randomPet = randomOption(1 ,6)
+    let setEnemyPet = document.getElementById('opponent-pet')
 
-    if (randomAtack ==1) {
-        sayEnemysPet.innerHTML = 'Hipogoge'
-    } else if(randomAtack == 2) {
-        sayEnemysPet.innerHTML = 'Capi'
-    } else if(randomAtack == 3) {
-        sayEnemysPet.innerHTML = 'Ratigueya'
-    } else if(randomAtack == 4) {
-        sayEnemysPet.innerHTML = 'Langos'
-    } else if(randomAtack == 5) {
-        sayEnemysPet.innerHTML = 'Tucapalma'
+    if (randomPet ==1) {
+        setEnemyPet.innerHTML = 'Hipogoge'
+    } else if(randomPet == 2) {
+        setEnemyPet.innerHTML = 'Capi'
+    } else if(randomPet == 3) {
+        setEnemyPet.innerHTML = 'Ratigueya'
+    } else if(randomPet == 4) {
+        setEnemyPet.innerHTML = 'Langos'
+    } else if(randomPet == 5) {
+        setEnemyPet.innerHTML = 'Tucapalma'
     } else {
-        sayEnemysPet.innerHTML = 'Pydos'
+        setEnemyPet.innerHTML = 'Pydos'
     }
+}
+
+function waterAtac() {
+    gamerAtac = 'Water'
+    alert("You attacked with " + gamerAtac)
+    startEnemyAtac()
+}
+
+function fireAtac(){
+    gamerAtac = 'Fire'
+    alert("You attacked with " + gamerAtac)
+    startEnemyAtac()
+
+}
+
+function earthAtac(){
+    gamerAtac = 'Earth'
+    alert("You attacked with " + gamerAtac)
+    startEnemyAtac()
+}
+
+function airAtac(){
+    gamerAtac = 'Air'
+    alert("You attacked with " + gamerAtac)
+    startEnemyAtac()
+}
+
+function eterAtac(){
+    gamerAtac = 'Eter'
+    alert("You attacked with " + gamerAtac)
+    startEnemyAtac()
+}
+
+function gasAtac(){
+    gamerAtac = 'Gas'
+    alert("You attacked with " + gamerAtac)
+    startEnemyAtac()
 }
 
 function randomOption(min, max) {

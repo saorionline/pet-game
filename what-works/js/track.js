5fb443fec29ebd0dc08ba38d3941b1682e9d18dd
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-10 21:45:24 -0500

    just styling

--- what-works/js/track.js
+++ what-works/js/track.js
@@ -1,25 +1,40 @@
 let gamerAtac 
 let enemyPower 
+let lifeCounter = 3
+let robotLife = 3
     
 function startGame(){
-    let petPress = document.getElementById('pet-press')
-petPress.addEventListener('click', gamerOption)
+    let hideReload = document.getElementById('reload')
+    hideReload.style.display = 'none'
 
-let waterPower = document.getElementById('water-push')
-waterPower.addEventListener('click', waterAtac)
-let firePower = document.getElementById('fire-push')
-firePower.addEventListener('click', fireAtac)
-let earthPower = document.getElementById('earth-push')
-earthPower.addEventListener('click', earthAtac)
-/* let airPower = document.getElementById('air-storm')
-airPower.addEventListener('click', airAtac)
-let eterPower = document.getElementById('eter-soft')
-eterPower.addEventListener('click', eterAtac)
-let gasPower = document.getElementById('gas-release')
-gasPower.addEventListener('click', gasAtac) */
+    let grabHiddenSection = document.getElementById('select-ataq')
+    grabHiddenSection.style.display = 'none'
+
+    let petPress = document.getElementById('pet-press')
+    petPress.addEventListener('click', gamerOption)
+    let waterPower = document.getElementById('water-push')
+    waterPower.addEventListener('click', waterAtac)
+    let firePower = document.getElementById('fire-push')
+    firePower.addEventListener('click', fireAtac)
+    let earthPower = document.getElementById('earth-push')
+    earthPower.addEventListener('click', earthAtac)
+    /* let airPower = document.getElementById('air-storm')
+    airPower.addEventListener('click', airAtac)
+    let eterPower = document.getElementById('eter-soft')
+    eterPower.addEventListener('click', eterAtac)
+    let gasPower = document.getElementById('gas-release')
+    gasPower.addEventListener('click', gasAtac) */
+    let reloadButton = document.getElementById('game-push')
+    reloadButton.addEventListener('click', reloadAgain)     
 }
 
 function gamerOption() {
+    let hidePet = document.getElementById('select-pet')
+    hidePet.style.display = 'none'
+
+    let grabHiddenSection = document.getElementById('select-ataq')
+    grabHiddenSection.style.display = 'block'
+
     let inputHipogoge = document.getElementById('hipogoge')
     let inputCapi = document.getElementById('capi')
     let inputRatigueya = document.getElementById('ratigueya')
@@ -46,7 +61,7 @@ function gamerOption() {
    enemyPet()
 }
 
-function enemyPet(){
+function enemyPet() {
     let randomPet = randomOption(1 ,6)
     let setEnemyPet = document.getElementById('opponent-pet')
 
@@ -70,10 +85,14 @@ function waterAtac() {
     startEnemyAtac()
 }
 
-function fireAtac(){
+function fireAtac() {
     gamerAtac = 'Fire'
     startEnemyAtac()
 
+}
+
+function reloadAgain() {
+    location.reload()
 }
 
 function earthAtac(){
@@ -124,22 +143,42 @@ function startEnemyAtac() {
 
    //START FIGHT!
 function fight() {
+    let spanLifeCounter = document.getElementById('gamer-life')
+    let spanRobotLife = document.getElementById('robot-life')
+
     if(gamerAtac == enemyPower){
-        announceWinner("match ")
+        announceWinner(" match ")
     } else if (gamerAtac == 'Water' && enemyPower == 'Fire' ) {
-        announceWinner("beat ")
-        // countWin = countWin + 1
+        announceWinner(" beat ")
+        robotLife--
+        spanRobotLife.innerHTML = robotLife
     } else if (gamerAtac == 'Earth' && enemyPower == 'Water' ) {
-        announceWinner("beat ")
-        // countWin = countWin + 1
+        announceWinner(" beat ")
+        robotLife--
+        spanRobotLife.innerHTML = robotLife
     }else if (gamerAtac == 'Fire' && enemyPower == 'Earth' ) {
-        announceWinner("beat ")
+        announceWinner(" beat ")
+        robotLife--
+        spanRobotLife.innerHTML = robotLife
         //countWin = countWin + 1
     } else {
-        announceWinner("lost ")
+        announceWinner(" lost ")
+        lifeCounter--
+        spanLifeCounter.innerHTML = lifeCounter
         //countLost = countLost + 1
     }
-}
+    lifeDoc()}
+
+function lifeDoc() {
+    let showReload = document.getElementById('reload')
+    showReload.style.display = 'block'
+
+    if(robotLife == 0) {
+        endGame("Crack")
+    } else if (lifeCounter == 0) {
+        endGame("Game Over")
+    } 
+}    
 
 function announceWinner(resultFight) {
     let singResult = document.getElementById('message')
@@ -149,6 +188,22 @@ function announceWinner(resultFight) {
 
     singResult.appendChild(showCase)
 }
+
+function endGame(deadMark) {
+    let singEnd = document.getElementById('message')
+
+    let showEnd = document.createElement('p')
+    showEnd.innerHTML = deadMark
+
+    singEnd.appendChild(showEnd)
+    let waterPower = document.getElementById('water-push')
+    waterPower.disabled = true
+    let firePower = document.getElementById('fire-push')
+    firePower.disabled = true 
+    let earthPower = document.getElementById('earth-push')
+    earthPower.disabled = true 
+}
+
 function randomOption(min, max) {
     return Math.floor(Math.random() * (max-min+1)+min)
 }

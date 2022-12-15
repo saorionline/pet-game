dfb7616a8fecb0f4a7f08afd5efe9d37e02bde5f
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-15 17:53:10 -0500

    almost to the basic version

--- what-works/js/track.js
+++ what-works/js/track.js
@@ -1,238 +1,117 @@
-//Reload
-
-//LifeDoc
-const showReload = document.getElementById('reload')
+//ataqueEnemigo
+let enemyPower
+//ataqueJugador del que le da el poder a la mascota
+let gamerAtac 
+//vidasJugador
+let lifeCounter = 3
+//vidasEnemigo
+let robotLife = 3
 //Announcer
 const singResult = document.getElementById('board')
-//Fight
-const spanLifeCounter = document.getElementById('gamer-life')
-const spanRobotLife = document.getElementById('robot-life')
-// Select Pet
-const hidePet = document.getElementById('choose-pet')
-const grabHiddenSection = document.getElementById('select-ataq')
+
 // Select Power
-const hideReload = document.getElementById('reload')
-const petPress = document.getElementById('pet-press')
-const reloadButton = document.getElementById('game-push')   
-
-
+const hideReload = document.getElementById('reload')  
 // Name Pet 
 const setGamerPet = document.getElementById('gamer-pet')
-const setEnemyPet = document.getElementById('opponent-pet')
 
-// Start Game and Select Pet first part main variable
-const cardsBox = document.getElementById('card-box-code')
-const boxPowerButtons = document.getElementById('push-box')
-//Other Variables
 
-let extractAtacks
-let petPlayer
-let superPets=[]
-let gamerAtac 
-let enemyPower = []
-
-let gamerOneByOne = []
-let buttonDefend = []
-let waterPower 
-let firePower 
-let earthPower 
-
-let infoAccordion
-let inputHipogoge 
-let inputCapi 
-let inputRatigueya 
-
-let lifeCounter = 3
-let robotLife = 3
-
-class superPet {
-    constructor(name, photo, life) {
-        this.name = name
-        this.photo = photo
-        this.life = life
-        this.powers = []
-    }
-}
-
-let hipogoge = new superPet('Hipogoge', './assets/hipogoge.png', 3)
-let capi = new superPet('Capi', './assets/capi.png', 3)
-let ratigueya = new superPet('Ratigueya', './assets/ratigueya.png', 3)
-
-hipogoge.powers.push(
-    {
-        name: '💧' , 
-        id: 'water-push'
-    },
-    {
-        name: '🔥', 
-        id: 'fire-push'
-    },
-    {
-        name: '🌱', 
-        id: 'earth-push'
-    }
-)
-capi.powers.push(
-    {
-        name: '💧', 
-        id: 'water-push'
-    },
-    {
-        name: '🔥', 
-        id: 'fire-push'
-    },
-    {
-        name: '🌱', 
-        id: 'earth-push'
-    }
-)
-
-ratigueya.powers.push(
-    {
-        name: '💧', 
-        id: 'water-push'
-    },
-    {
-        name: '🔥', 
-        id: 'fire-push'
-    },
-    {
-        name: '🌱', 
-        id: 'earth-push'
-    }
-)
-
-superPets.push( hipogoge, capi, ratigueya)
-    
+//iniciarJuego
 function startGame(){
+    // sectionSeleccionarAtaque
+    const grabHiddenSection = document.getElementById('select-ataq')
     grabHiddenSection.style.display = 'none'
-
-    superPets.forEach((superPet) => {
-        infoAccordion = `
-        <input type="radio" name="pet" id=${superPet.name} />
-        <label class= "pet-card" for=${superPet.name} >
-            <img src=${superPet.photo} alt=${superPet.name} />
-        </label>
-        `
-        cardsBox.innerHTML += infoAccordion
-
-        inputHipogoge = document.getElementById('Hipogoge')
-        inputCapi = document.getElementById('Capi')
-        inputRatigueya = document.getElementById('Ratigueya')
-    })
-
+    //sectionReiniciar
+    const showReload = document.getElementById('reload')
+    showReload.style.display = 'none'
+    //botonMascotaJugador --- boton-mascota
+    let petPress = document.getElementById('pet-press')
     petPress.addEventListener('click', gamerOption)
+    //botonAgua -- boton-agua
+    let waterPower = document.getElementById('water-push')
+    waterPower.addEventListener('click', waterAtac)
+
+    //botonReiniciar
+    const reloadButton = document.getElementById('game-push') 
     reloadButton.addEventListener('click', reloadAgain)     
 }
 
+//seleccionarMascotaJugador
 function gamerOption() {
+        // Select Pet //seleccionarMascotaJugador //gamerOption // seleccionar-mascota
+    const hidePet = document.getElementById('choose-pet')
     hidePet.style.display = 'none'
+    //sectionSeleccionarAtaque --- seleccionar-ataque
+    const grabHiddenSection = document.getElementById('select-ataq')
     grabHiddenSection.style.display = 'flex'
 
+    let inputHipogoge = document.getElementById('Hipogoge')
+    
    if(inputHipogoge.checked) {
+    //spanMascotaJugador
     setGamerPet.innerHTML = inputHipogoge.id
-    petPlayer = inputHipogoge.id
-   } else if (inputCapi.checked) {
-    setGamerPet.innerHTML = inputCapi.id
-    petPlayer = inputCapi.id
-   } else if (inputRatigueya.checked) {
-    setGamerPet.innerHTML = inputRatigueya.id
-    petPlayer = inputRatigueya.id
    } else {
     alert ('Please select a Pet')
    }  
-   bringAtacks(petPlayer)
+//seleccionarMascotaEnemigo
+
    enemyPet()
 }
-function bringAtacks(petPlayer){
-    let powers
-    for (let index = 0; index < superPets.length; index++){
-        if(petPlayer === superPets[index].name) {
-            powers = superPets[index].powers
-        }
-    }
-    powersCase(powers)
-}
-function powersCase(powers) {
-    powers.forEach((power) => {
-        extractAtacks = `
-        <button id=${power.id} class="power defend">${power.name}</button>
-        `
-        boxPowerButtons.innerHTML += extractAtacks
-    })
 
-    buttonDefend = document.querySelectorAll('defend')
-    waterPower = document.getElementById('water-push')
-    firePower = document.getElementById('fire-push')
-    earthPower = document.getElementById('earth-push')
-
-
-}
-
-function oneByOne() {
-    buttonDefend.forEach((defendButton) => {
-        defendButton.addEventListener('click', (e) => {
-            if(e.target.textContent === '🔥') {
-                gamerOneByOne.push('Fire')
-                console.log(gamerOneByOne)
-                defendButton.style.background ='#112f58'
-            } else if (e.target.textContent === '💧') {
-                gamerOneByOne.push('Water')
-                console.log(gamerOneByOne)
-                defendButton.style.background ='#112f58'
-            } else {
-                gamerOneByOne.push('Earth')
-                console.log(gamerOneByOne)
-                defendButton.style.background ='#112f58'
-            }
-            startEnemyAtac()
-        })
-    })
-}
-
+//seleccionarMascotaEnemigo
 function enemyPet() {
-    let randomPet = randomOption(0 , superPets.length - 1)
-    setEnemyPet.innerHTML = superPets[randomPet].name
-    enemyPower = superPets[randomPet].powers
-    oneByOne()
+    //mascotaAleatoria
+    let randomPet = randomOption(1, 3)
+    //spanMascotaEnemigo y mascota-enemigo
+    const setEnemyPet = document.getElementById('opponent-pet')
+
+    // mascotaAleatoria -- spanMascotaEnemigo 
+    if (randomPet == 1) {
+        //spanMascotaEnemigo
+        setEnemyPet.innerHTML = 'Hipogoge'
+    } else {
+        alert('You won by default!')
+    }
+}
+//ataqueAgua ataqueJugador
+function waterAtac() {
+    gamerAtac = 'Water'
+    startEnemyAtac()
+}
+//ataqueAleatorioEnemigo --- aleatorio --- ataqueEnemigo
+function startEnemyAtac() {
+    //ataqueAleatorio
+    let randomAtac = randomOption(1, 3)
+    if (randomAtac == 1) {
+        enemyPower = 'Water'
+    } else {
+        alert('The ods are in your favor')
+    }  
+    fight()
 }
 
-
-function startEnemyAtac() {
-    let randomAtac = randomOption(0 , enemyPower.lenght -1)
-    if (randomAtac == 0 || randomAtac == 1) {
-        enemyPower.push('Water')
-    } else if(randomAtac == 3 || randomAtac == 4) {
-        enemyPower.push('Fire')
-    } else {
-        enemyPower.push('Earth')
-    }  
-    console.log(enemyPower)
-    fight()}
-
-   //START FIGHT!
+   //START FIGHT! -- ataqueJugador 
 function fight() {
+    // spanVidasJugador -- vidas-jugador
+    const spanLifeCounter = document.getElementById('gamer-life')
+    // spanVidasEnemigo -- vidas-enemigo
+    const spanRobotLife = document.getElementById('robot-life')
+    //ataqueJugador -- ataqueEnemigo
     if(gamerAtac == enemyPower){
+        //crearMensaje
         announceWinner(" match ")
-    } else if (gamerAtac == 'Water' && enemyPower == 'Fire' ) {
+    } else if (gamerAtac == 'Water' && enemyPower != 'Water' ) {
         announceWinner(" beat ")
+        //vidasEnemigo
         robotLife--
         spanRobotLife.innerHTML = robotLife
-    } else if (gamerAtac == 'Earth' && enemyPower == 'Water' ) {
-        announceWinner(" beat ")
-        robotLife--
-        spanRobotLife.innerHTML = robotLife
-    }else if (gamerAtac == 'Fire' && enemyPower == 'Earth' ) {
-        announceWinner(" beat ")
-        robotLife--
-        spanRobotLife.innerHTML = robotLife
-        //countWin = countWin + 1
     } else {
         announceWinner(" lost ")
+        //vidasJugador
         lifeCounter--
         spanLifeCounter.innerHTML = lifeCounter
-        //countLost = countLost + 1
+        
     }
+    //revisarVidas
     lifeDoc()}
 
 function lifeDoc() {
@@ -244,7 +123,7 @@ function lifeDoc() {
         endGame("Game Over")
     } 
 }    
-
+// crearMensajeFina(resultadoFinal)
 function endGame(deadMark) {
 
     let showEnd = document.createElement('p')
@@ -258,6 +137,7 @@ function endGame(deadMark) {
 function reloadAgain() {
     location.reload()
 }
+// crearMensaje(resultado)
 function announceWinner(resultFight) {
 
     let showCase = document.createElement('p')
